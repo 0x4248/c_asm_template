6afb80c1fd23f0134c8166f034a9123310080f96
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-10 21:59:27 +0100

    Add comments to config.mk
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -2,6 +2,9 @@
 # Github: https://www.github.com/awesomelewis2007/c_asm_template
 # Main config file for C ASM Template
 
+# Target
+# For x86_64 put in ARCH = x86_64
+# For aarch64 put in ARCH = aarch64
 ARCH = x86_64
 
 CC = gcc
